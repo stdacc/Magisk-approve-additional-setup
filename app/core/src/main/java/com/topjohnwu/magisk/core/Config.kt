143a7641a703d50648cd4549dce21c30c177d3d0
stdacc <157672281+stdacc@users.noreply.github.com>
2026-01-21 16:05:23 +0300

    Change suAutoResponse default to SU_AUTO_ALLOW
    Auto allow all su request

--- app/core/src/main/java/com/topjohnwu/magisk/core/Config.kt
+++ app/core/src/main/java/com/topjohnwu/magisk/core/Config.kt
@@ -135,7 +135,7 @@ object Config : PreferenceConfig, DBConfig {
     var keyStoreRaw by dbStrings(Key.KEYSTORE, "", true)
 
     var suDefaultTimeout by preferenceStrInt(Key.SU_REQUEST_TIMEOUT, 10)
-    var suAutoResponse by preferenceStrInt(Key.SU_AUTO_RESPONSE, Value.SU_PROMPT)
+    var suAutoResponse by preferenceStrInt(Key.SU_AUTO_RESPONSE, Value.SU_AUTO_ALLOW)
     var suNotification by preferenceStrInt(Key.SU_NOTIFICATION, Value.NOTIFICATION_TOAST)
     var rootMode by dbSettings(Key.ROOT_ACCESS, Value.ROOT_ACCESS_APPS_AND_ADB)
     var suMntNamespaceMode by dbSettings(Key.SU_MNT_NS, Value.NAMESPACE_MODE_REQUESTER)
